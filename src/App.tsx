ebd0fcd61e30d710373f822f567cfa7ec15ae61a
a.spatharas <aggelos.spatharas.easyfix@gmail.com>
2025-08-22 20:32:08 +0300

    feat: add Footer component and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 
 function App() {
   return (
     <main className="container">
       <Hero />
+      <Footer />
     </main>
   );
 }
